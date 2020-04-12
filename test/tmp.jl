ac3246e9b591d89b72f38a6429f238fd1e764dea
Vidhya <vidhyasaharan@gmail.com>
2020-04-12 14:29:29 +1000

    Added temporary file for test and development code

---
+++ test/tmp.jl
@@ -0,0 +1,7 @@
+using WAV
+
+testdir = normpath(joinpath(dirname(@__FILE__),"../test/"))
+x, fs = wavread(joinpath(testdir,"King.wav"))
+
+frames = framed_signal(x,fs,0.09,0.01)
+frame = extract_frame(frames,11)

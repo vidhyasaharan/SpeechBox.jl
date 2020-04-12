0fb0cb250ed71b3ce790b3fd1ea826a36419c359
Vidhya <vidhyasaharan@gmail.com>
2020-04-12 14:30:07 +1000

    Added functions for generating complex exponential basis matrix

--- src/internal_utilities.jl
+++ src/internal_utilities.jl
@@ -1,3 +1,4 @@
+#Generate window function of given length
 function window(flen::Int;wtype::String="hanning")
     if(wtype=="rect")
         win = ones(flen)
@@ -11,3 +12,15 @@ function window(flen::Int;wtype::String="hanning")
     end
     return win
 end
+
+#Generate array of frequencies (in Hz), equally spaced in log domain with resolution given in frequencies per octave
+function logfreq_array(;fmin::Number = 10, fmax::Number = 4000, frq_per_octave::Number = 120)
+    lfmin = log2(fmin)
+    lfmax = log2(fmax)
+    lfres = 1/frq_per_octave
+    lfrq = lfmin:lfres:lfmax
+    return exp2.(lfrq)
+end
+
+#Generate complex exponential sequence
+cexp(f,fs,N) = (1/sqrt(N))*exp.(2π*im*(f/fs)*(1:N))

7ab274ce6a1aca2a53f7a7f4775ee21f2c758d26
Vidhya <vidhyasaharan@gmail.com>
2020-04-12 17:47:25 +1000

    Added functions for complex exponentials projection matrix generation

--- src/internal_utilities.jl
+++ src/internal_utilities.jl
@@ -23,14 +23,14 @@ function logfreq_array(;fmin::Number = 10, fmax::Number = 4000, frq_per_octave::
 end
 
 #Generate complex exponential sequence
-cexp(f,fs,N) = (1/sqrt(N))*exp.(2π*im*(f/fs)*(1:N))
+cexp(f,fs,N) = exp.(2π*im*(f/fs)*(1:N))
 
 #Generate projection matrix for complex exponential signals/vectors
-function periodogram_basis_matrix(frqs::Array{T,1},fs::Number,N::Int) where T<:Number
+function cexp_proj_matrix(frqs::Array{T,1},fs::Number,N::Int) where T<:Number
     nfrqs = length(frqs)
-    periodogram_basis = zeros(Complex{Float},nfrqs,N)
+    proj_matrix = zeros(Complex{Float},nfrqs,N)
     for i=1:nfrqs
-        periodogram_basis[i,:] = cexp(frqs[i],fs,N)
+        proj_matrix[i,:] = cexp(frqs[i],fs,N)
     end
-    return periodogram_basis
+    return proj_matrix
 end

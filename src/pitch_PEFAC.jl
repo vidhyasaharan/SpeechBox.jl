efc7b8e0fe571b6498620718dbd155262707b112
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2020-10-19 00:17:32 +1100

    Added functions for moving_average and LTASS_normalise - incomplete

--- src/pitch_PEFAC.jl
+++ src/pitch_PEFAC.jl
@@ -37,9 +37,6 @@ function generate_pitch_estimate(x::Array{Float64,1},fs::Number)
     return y
 end
 
-
-
-
 function LTASS(frqs::Vector{T}) where T<:Real
     f = [63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000]
     m = [38.6, 43.5, 54.4, 57.7, 56.8, 60.2, 60.3, 59.0, 62.1, 62.1, 60.5, 56.8, 53.7, 53.0, 52.0, 48.7, 48.1, 46.8, 45.6, 44.5, 44.3, 43.7, 43.4, 41.3, 40.7]
@@ -51,6 +48,50 @@ function LTASS(frqs::Vector{T}) where T<:Real
     return ltass_mag
 end
 
+function LTASS(fmax::Number, frq_per_octave::Number)
+    f = [63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000]
+    m = [38.6, 43.5, 54.4, 57.7, 56.8, 60.2, 60.3, 59.0, 62.1, 62.1, 60.5, 56.8, 53.7, 53.0, 52.0, 48.7, 48.1, 46.8, 45.6, 44.5, 44.3, 43.7, 43.4, 41.3, 40.7]
+    itp = interpolate((f,),m,Gridded(Linear()))
+    frqs = logfreq_array(fmin = f[1], fmax = min(fmax,f[end]), frq_per_octave = 1000)
+    ltass_mag = zeros(size(frqs))
+    for i=1:length(frqs)
+        ltass_mag[i] = itp(frqs[i])
+    end
+    return ltass_mag, frqs
+end
 
-lf = SpeechBox.logfreq_array(fmin = 63, fmax = 16000, frq_per_octave = 1000)
-lt = LTASS(lf)
+
+
+function moving_average(pd::timefreq; time_window::Int, freq_window::Int)
+    tfilt = DSP.Filters.PolynomialRatio(1/time_window*ones(time_window),[1])
+    ffilt = DSP.Filters.PolynomialRatio(1/freq_window*ones(freq_window),[1])
+    smoothed_components = zeros(size(pd.components))
+    nfrqs,nframes = size(pd.components)
+    time_pad_len = Int((time_window-1)/2)
+    freq_pad_len = Int((freq_window-1)/2)
+    for i=1:nfrqs
+        ip = symmetric_pad(pd.components[i,:],time_pad_len)
+        op = DSP.filt(tfilt,ip)
+        smoothed_components[i,:] = unpad_vector(op,time_pad_len)
+    end
+    for i=1:nframes
+        ip = symmetric_pad(smoothed_components[:,i],freq_pad_len)
+        op = DSP.filt(ffilt,ip)
+        smoothed_components[:,i] = unpad_vector(op,freq_pad_len)
+    end
+    return timefreq(pd.frames,smoothed_components,pd.frqs)
+end
+
+
+function LTASS_normalise(pd::timefreq)
+    time_window = 11
+    freq_window = 11
+    frq_per_octave = 1000
+    ltass_mag = LTASS(pd.frqs)
+    smpd = moving_average(pd,time_window = time_window, freq_window = freq_window)
+    norm_components = zeros(size(pd.components))
+    for i=1:size(pd.components,2)
+        norm_components[:,i] = pd.components[:,i].*ltass_mag./smpd.components[:,i]
+    end
+    return timefreq(pd.frames,norm_components,pd.frqs)
+end
--- test/pitch_PEFAC_tests.jl
+++ test/pitch_PEFAC_tests.jl
@@ -10,5 +10,25 @@ signal = speech_waveform(x,fs)
 sig_frames = framed_signal(signal)
 sframe = extract_frame(sig_frames,52)
 
+
 frqs = SpeechBox.logfreq_array(fmin = 1, fmax = fs/2, frq_per_octave = 1000)
-pd = periodogram(sframe,fs,frqs)
+
+ltass_mag,frqs = SpeechBox.LTASS(fs/2,1000)
+pd = SpeechBox.periodogram(sig_frames,frqs)
+sm = SpeechBox.moving_average(pow2db(pd),time_window = 11, freq_window = 11)
+lpd = pow2db(pd)
+
+
+fnum = 55
+tt = lpd.components[:,fnum].*ltass_mag./sm.components[:,fnum]
+plot(tt)
+
+plot(sm)
+plot(lpd)
+
+
+npd = SpeechBox.LTASS_normalise(lpd)
+plot(npd)
+plot(npd.components[:,149])
+
+tt = maximum(lpd.components)

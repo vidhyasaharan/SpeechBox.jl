a5616ac3836a49a092244416e2987e5c6eb1faea
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2020-11-15 15:41:55 +1100

    Modified test since cexp() was changed to include normalisation of cexp

--- test/internal_utilities_tests.jl
+++ test/internal_utilities_tests.jl
@@ -62,11 +62,11 @@ end
 
     for f in frqs
         f = 100
-        ce = SpeechBox.cexp(f,fs,N)
+        ce = sqrt(N)*SpeechBox.cexp(f,fs,N)
         @test ce⋅ce ≈ N
         for i=1:N
-            @test isapprox(real(ce[i]),cs(f,i);atol = 1e-10)
-            @test isapprox(imag(ce[i]),sn(f,i);atol = 1e-10)
+            @test isapprox(real(ce[i]),cs(f,i);atol = 1e-6)
+            @test isapprox(imag(ce[i]),-sn(f,i);atol = 1e-6)
         end
     end
 end

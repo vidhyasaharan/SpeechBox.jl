7e5e15d65e0ff0be967a739092cfe6000df49d80
vidhyasaharan <vidhyasaharan@gmail.com>
2021-04-15 23:56:52 +1000

    more lpc tests

--- test/lpc_tests.jl
+++ test/lpc_tests.jl
@@ -7,11 +7,9 @@ end
 
 
 @testset "lpc" begin
-
     c = 0.8
     d = 3
     N = 5
-
     a = zeros(d+1)
     a[1] = 1.0
     a[d+1] = c
@@ -29,5 +27,21 @@ end
         if i≠(d+1)
             @test abs(coeff[i]) < 0.1*c
         end
+    end
+end
+
+
+@testset "lpc_freqz" begin
+    fs = 16000
+    nsam = 10000
+    ar,f,bw = SpeechBox.rand_allpole(fs, 1)
+    x = filt(ar, SpeechBox.white_noise(nsam))
+    
+    nfrqs = 10
+    frqs = [f; rand(nfrqs)*(fs/2)]
+    N = 4
+    h = SpeechBox.lpc_freqz(x,fs,N;frqs)
+    for i=2:length(h)
+        @test abs(h[1]) > abs(h[i])
     end
 end

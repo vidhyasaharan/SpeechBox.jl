5d3df633a62d770c643ed6d3f4f43654b850adb8
vidhyasaharan <vidhyasaharan@gmail.com>
2021-04-15 23:24:50 +1000

    cleaner code in lpc test

--- test/lpc_tests.jl
+++ test/lpc_tests.jl
@@ -7,23 +7,27 @@ end
 
 
 @testset "lpc" begin
-    rng = MersenneTwister(100)
-    x = randn(rng,Float64,(10000))
-    a = 0.8
+
+    c = 0.8
     d = 3
     N = 5
-    for i=d+1:length(x)
-        x[i] += a*x[i-d]
-    end
+
+    a = zeros(d+1)
+    a[1] = 1.0
+    a[d+1] = c
+
+    x = SpeechBox.ar_process(a, 10000)
+
     coeff = lpc(x,N)
+
     @test length(coeff) == N+1
     @test coeff[1] == 1.0
     
-    @test coeff[d+1] > -1.1*a
-    @test coeff[d+1] < -0.9*a
+    @test coeff[d+1] < 1.1*c
+    @test coeff[d+1] > 0.9*c
     for i=2:length(coeff)
         if i≠(d+1)
-            @test abs(coeff[i]) < 0.1*a
+            @test abs(coeff[i]) < 0.1*c
         end
     end
 end

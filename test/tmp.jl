8fab3bed9c4f272d972394411b24f90317a65fa6
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-05-10 23:59:09 +1000

    added SA1.wav from TIMIT

--- test/tmp.jl
+++ test/tmp.jl
@@ -4,7 +4,7 @@ using Plots
 using BenchmarkTools
 
 testdir = normpath(joinpath(dirname(pathof(SpeechBox)),"../test/"))
-x, fs = wavread(joinpath(testdir,"King.wav"))
+x, fs = wavread(joinpath(testdir,"SA1.wav"))
 
 frames = framed_signal(x,fs,0.05,0.01)
 frame = extract_frame(frames,11)

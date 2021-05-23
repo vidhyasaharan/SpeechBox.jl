9a85469617429e5016a0fdbb78b39b3326861c1c
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-05-23 17:18:59 +1000

    added tests for xcorr

--- test/loopvectorized_utilities_tests.jl
+++ test/loopvectorized_utilities_tests.jl
@@ -9,3 +9,17 @@
     @test ca⋅b ≈ SpeechBox.dotavx(ca,b)
     @test ca⋅cb ≈ SpeechBox.dotavx(ca,cb)
 end
+
+@testset "xcorr" begin
+    len = 1000
+    hlen = 100
+    hstart = 101
+    x = randn(Float,len)
+    h = x[hstart:hstart+hlen-1]
+    z = 10
+    y = SpeechBox.xcorr(x,h,z)
+    @test typeof(y) == Vector{Float}
+    @test length(y) == length(x)
+    @test argmax(y) == hstart + z - 1
+    @test maximum(y) == SpeechBox.dotavx(h)
+end

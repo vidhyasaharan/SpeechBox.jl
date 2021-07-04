c668205f7632b8278c9de7c154fec488e2c26590
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-07-04 19:36:36 +1000

    modified for testing RAPT

--- test/tmp.jl
+++ test/tmp.jl
@@ -15,15 +15,20 @@ s = speech_waveform(x,fs)
 frames = framed_signal(x,fs,0.05,0.01)
 frame = extract_frame(frames,11)
 
-msp = SpeechBox.specgram(frames)
-pp = SpeechBox.pitch_spec_comb(frames)
+ptf = SpeechBox.RAPT_timefreq(s)
+ca = SpeechBox.RAPT_pitch_candidates(s)
+lc = SpeechBox.RAPT_cands_local_costs(ca)
 
-cr, frqs = SpeechBox.xcorr_spectral_comb(frames)
-h,z = SpeechBox.generate_logfrq_pitch_comb(;frq_per_octave=200)
-pd = SpeechBox.periodogram(frames,frqs)
-lpd = log.(pd.components)
 
-fnum = 145
-ii,mm = SpeechBox.findpeaks_sorted(cr[:,fnum])
-p = plot(cr[:,fnum])
-plot!(p,ii,mm, seriestype=:scatter)
+# msp = SpeechBox.specgram(frames)
+# pp = SpeechBox.pitch_spec_comb(frames)
+
+# cr, frqs = SpeechBox.xcorr_spectral_comb(frames)
+# h,z = SpeechBox.generate_logfrq_pitch_comb(;frq_per_octave=200)
+# pd = SpeechBox.periodogram(frames,frqs)
+# lpd = log.(pd.components)
+
+# fnum = 145
+# ii,mm = SpeechBox.findpeaks_sorted(cr[:,fnum])
+# p = plot(cr[:,fnum])
+# plot!(p,ii,mm, seriestype=:scatter)

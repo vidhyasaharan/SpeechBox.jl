a7315542b301a2901065fe0fb041992d58e9f121
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-07-11 00:37:22 +1000

    minor edits

--- src/levinson_durbin.jl
+++ src/levinson_durbin.jl
@@ -24,17 +24,17 @@ end
 
 function lpc_LD!(α::AbstractVector{Float}, x::AbstractVector{Float})
     p = length(α)
-    rxx = SpeechBox.acorr(x, p+1)
+    rxx = acorr(x, p+1)
     E = Vector{Float}(undef,p+1)
     k = Vector{Float}(undef,p)
     @views E[1] = rxx[1]
     @views k[1] = rxx[2]/rxx[1]
     @views α[p] = k[1]
-    SpeechBox.update_E!(E,k,1)
+    update_E!(E,k,1)
     for i ∈ 2:p
-        SpeechBox.update_k!(k, α, rxx, E, i)
-        SpeechBox.update_α!(α, k, i)
-        SpeechBox.update_E!(E, k, i)
+        update_k!(k, α, rxx, E, i)
+        update_α!(α, k, i)
+        update_E!(E, k, i)
     end
 end
 
@@ -44,7 +44,7 @@ end
 
 function update_k!(k::AbstractVector{Float}, α::AbstractVector{Float}, rxx::AbstractVector{Float}, E::AbstractVector{Float}, iter::Int)
     @views a = α[end-iter+2:end]
-    @views k[iter] = (rxx[iter+1] - SpeechBox.dotavx(a,rxx[2:iter]))/E[iter]
+    @views k[iter] = (rxx[iter+1] - dotavx(a,rxx[2:iter]))/E[iter]
 end
 
 function update_α!(α::AbstractVector{Float}, k::AbstractVector{Float}, iter::Int)

8fc7d0e68ed9617132ff9463cfa4848db457d513
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-07-24 12:36:38 +1000

    introduced view_frame function

--- src/speechframing.jl
+++ src/speechframing.jl
@@ -5,21 +5,33 @@
 Extract frame number `i` from the the framed\\_signal object `x`
 
 """
-function extract_frame(x::framed_signal,i::Int)
+extract_frame(x::framed_signal,i::Int) = collect(view_frame(x, i))
+    
+
+
+# Function that creates a view of one frame as a subarray from framed_signal object
+"""
+    view_frame(x::framed_signal, i)
+
+Create of view of frame number `i` from the the framed\\_signal object `x` (Frames involving zero padding of the signal will be arrays not views).
+
+"""
+function view_frame(x::framed_signal,i::Int)
     frame_length = x.frame_length
     frame_shift = x.frame_shift
     sindx = (i-1)*frame_shift + 1 #Identify start index of desired frame
     eindx = sindx + frame_length - 1 #Identify end index of desired frame
     if(i<=x.num_signal_frames) #Checking to see end of frame is within bounds of defined signal
-        frame = collect(x.signal.x[sindx:eindx])
+        frame = @view x.signal.x[sindx:eindx]
     else #Zero padding to return full frame if signal ends midway through the frame
-        frame = zeros(frame_length)
+        frame = zeros(Float,frame_length)
         lindx = length(x.signal.x)
         sig_len = lindx - sindx + 1
-        frame[1:sig_len] = collect(x.signal.x[sindx:lindx])
+        frame[1:sig_len] = @view x.signal.x[sindx:lindx]
     end
     return frame
 end
+
 
 
 #Compute the number of complete frames in signal (without zero padding) given frame size and frame shift

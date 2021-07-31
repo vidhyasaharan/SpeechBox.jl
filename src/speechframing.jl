b56be62398b397c46ff891010f3dd18219a949fe
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-07-31 19:48:53 +1000

    introduced enframe! and enframe

--- src/speechframing.jl
+++ src/speechframing.jl
@@ -1,3 +1,55 @@
+# Functions that fill a matrix of frames from a signal
+"""
+    enframe!(A, x)
+
+Fills each column of matrix `A` with a frame of array `x`. Frame size is given by number of rows of `A` and frame shift is determined such that the final frame is as close as possible to end of `x`.
+"""
+function enframe!(y::AbstractMatrix{Float}, x::AbstractVector{Float})
+    frame_size = size(y,1)
+    num_frames = size(y,2)
+    len = length(x)
+    frame_shift = Int(floor((len - frame_size)/(num_frames-1)))
+    for i ∈ 1:num_frames
+        sindx = (i-1)*frame_shift + 1
+        eindx = sindx + frame_size - 1
+        y[:,i] = x[sindx:eindx]
+    end
+    return y
+end
+
+
+"""
+    enframe(x, frame_size, frame_shift)
+
+Generate a matrix with each column storing one frame of length `frame_size` from signal in array `x`, with consecutive frames separated by `frame_shift` samples.
+"""
+function enframe(x::AbstractVector{Float}, frame_size::Int, frame_shift::Int)
+    num_frames = number_signal_frames(x, frame_size, frame_shift)
+    y = Matrix{Float}(undef,frame_size,num_frames)
+    enframe!(y, x)
+    return y
+end
+
+"""
+    enframe(x, fs, frame_dur, frame_shift_dur)
+
+Generate a matrix with each column storing one frame of duration `frame_dur` in seconds from signal in array `x` with sampling rate `fs`, with consecutive frames separated by `frame_shift_dur` in seconds.
+"""
+function enframe(x::AbstractVector{Float}, fs::Real, frame_dur::Real, frame_shift_dur::Real)
+    frame_size = time2nsamples(frame_dur,fs)
+    frame_shift = time2nsamples(frame_shift_dur,fs)
+    return enframe(x,frame_size, frame_shift)
+end
+
+"""
+    enframe(signal::speech_waveform, frame_dur, frame_shift_dur)
+
+Generate a matrix with each column storing one frame of duration `frame_dur` in seconds from speech\\_waveform `signal`, with consecutive frames separated by `frame_shift_dur` in seconds.
+"""
+enframe(s::speech_waveform, frame_dur::Real, frame_shift_dur::Real) = enframe(s.x, s.fs, frame_dur, frame_shift_dur)
+
+
+
 # Function that pulls out one frame as an array from framed_signal object
 """
     extract_frame(x::framed_signal, i)
@@ -36,12 +88,22 @@ end
 
 #Compute the number of complete frames in signal (without zero padding) given frame size and frame shift
 """
+    number_signal_frames(x, frame_size, frame_shift)
+    number_signal_frames(x, fs, frame_dur, frame_shift_dur)
     number_signal_frames(s::speech_waveform, frame_size, frame_shift)
     number_signal_frames(s::speech_waveform, frame_dur, frame_shift_dur)
 
-Compute the number of frames without zero padding or extension of speech waveform `s` given `frame_size` and `frame_shift` in number of samples or `frame_dur` and `frame_shift_dur` in seconds
+Compute the number of frames without zero padding or extension of array `x` or speech waveform `s` given `frame_size` and `frame_shift` in number of samples or `frame_dur` and `frame_shift_dur` in seconds
 """
-number_signal_frames(s::speech_waveform, frame_size::Int, frame_shift::Int) = 1+ Int(floor((length(s.x)-frame_size)/frame_shift))
+number_signal_frames(x::AbstractVector{Float}, frame_size::Int, frame_shift::Int) = 1+ Int(floor((length(x)-frame_size)/frame_shift))
+
+number_signal_frames(s::speech_waveform, frame_size::Int, frame_shift::Int) = number_signal_frames(s.x, frame_size, frame_shift)
+
+function number_signal_frames(x::AbstractVector{Float}, fs::Real, frame_dur::Float, frame_shift_dur::Float)
+    frame_size = time2nsamples(frame_dur, fs)
+    frame_shift = time2nsamples(frame_shift_dur, fs)
+    return number_signal_frames(x, frame_size, frame_shift)
+end
 
 function number_signal_frames(s::speech_waveform, frame_dur::Float, frame_shift_dur::Float)
     frame_size = time2nsamples(frame_dur,s.fs)

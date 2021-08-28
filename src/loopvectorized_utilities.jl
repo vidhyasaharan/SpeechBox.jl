3d10d40f4c564a0af3cfcc8773921bf66f33cd2b
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-08-29 00:08:46 +1000

    Introduced running_meanvar() function

--- src/loopvectorized_utilities.jl
+++ src/loopvectorized_utilities.jl
@@ -65,7 +65,7 @@ end
 
 #Running mean
 function running_mean(x::AbstractMatrix{T}) where T<:AbstractFloat
-    ndim,npts = size(x)
+    ndim = size(x,1)
     m = zeros(T,ndim)
     k = zero(T)
     for j ∈ axes(x,2)
@@ -78,6 +78,26 @@ function running_mean(x::AbstractMatrix{T}) where T<:AbstractFloat
     return m
 end
 
+#Running mean and variance
+function running_meanvar(x::AbstractMatrix{T}) where T<:AbstractFloat
+    ndim,npts = size(x)
+    m = zeros(T,ndim)
+    s = zeros(T,ndim)
+    k = zero(T)
+    for j ∈ axes(x,2)
+        k = 1/j
+        @turbo for i ∈ axes(x,1)
+            temp = (x[i,j] - m[i])
+            m[i] += temp*k
+            s[i] += temp*(x[i,j] - m[i])
+        end
+    end
+    N = 1/(npts-1)
+    @turbo for i ∈ eachindex(s)
+        s[i] *= N
+    end
+    return m, s
+end
 
 
 #Matrix multiplcation (in place for resultnant matrix)
--- test/loopvectorized_utilities_tests.jl
+++ test/loopvectorized_utilities_tests.jl
@@ -28,3 +28,12 @@ end
     x = randn(Float,ndim,npts)
     @test mean(x,dims=2) ≈ SpeechBox.running_mean(x)
 end
+
+@testset "running_meanvar" begin
+    ndim = 2
+    npts = 100000
+    x = randn(Float,ndim,npts)
+    m,v = SpeechBox.running_meanvar(x)
+    @test mean(x,dims=2) ≈ m
+    @test var(x,dims=2) ≈ v
+end

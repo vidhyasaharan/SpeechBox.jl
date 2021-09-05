f8ec9b6227cce6376d5b4bdedb268c8e2fa32ff3
Vidhyasaharan Sethu <vidhyasaharan@gmail.com>
2021-09-05 22:35:26 +1000

    Start implementing GMM tests

--- test/GMM_tests.jl
+++ test/GMM_tests.jl
@@ -1,22 +1,16 @@
-data = SpeechBox.generate_4_clusters(5)
-g = SpeechBox.GMMinit(4,data)
+@testset "normaliseWeights" begin
+    nmix = 10
+    w = rand(Float,nmix)
+    ŵ = copy(w)
+    SpeechBox.normaliseWeights!(w)
+    @test sum(w) ≈ one(Float)
+    for i ∈ eachindex(w,ŵ)
+        @test w[i]/sum(w) ≈ ŵ[i]/sum(ŵ)
+    end
+end
 
-p = scatter(data[1,:], data[2,:])
-scatter!(p,g.μ[1])
-scatter!(p,g.μ[2])
-scatter!(p,g.μ[3])
-scatter!(p,g.μ[4])
-
-
-t = log.(rand(Float64,10000))
-SpeechBox.logsumexp(t) ≈ log(sum(exp.(t)))
-@benchmark SpeechBox.logsumexp(t)
-
-
-x = randn(10,100000)
-m1 = Vector{Float64}(undef,size(x,1))
-v1 = Vector{Float64}(undef,size(x,1))
-m = Vector{Float64}(undef,size(x,1))
-C = Matrix{Float64}(undef,size(x,1),size(x,1))
-@benchmark SpeechBox.running_meanvar!(m1,v1,x)
-@benchmark SpeechBox.running_meancov!(m,C,x)
+# @testset "isconsistentX" begin
+#     nmix = 5
+#     ndim = 3
+#     w = rand(nmix)
+# end
